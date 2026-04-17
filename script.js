19d2cd6988ba41a83115c829ba2fa32bd88871c9
mayurjagtap4952 <jagtapmayur275@gmail.com>
2026-04-17 15:34:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function sendMessage(event) {
+  event.preventDefault();
+  alert("Message Sent Successfully!");
+}
+
+const text = "Hello, I'm Mayur Jagtap";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 80);
+  }
+}
+typing();
+
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
